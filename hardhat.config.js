bc113249cec24eaed0100ca43f63551c443720fe
wild-mouse <wildmouse0813@gmail.com>
2021-05-06 19:23:30 +0900

    Add mainnet forking

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config()
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -13,10 +14,20 @@ task("accounts", "Prints the list of accounts", async () => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
+const alchemyAPIKey = process.env.ALCHEMY_API_KEY
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.7.3",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${alchemyAPIKey}`,
+        blockNumber: 12380000
+      }
+    }
+  }
 };
 
